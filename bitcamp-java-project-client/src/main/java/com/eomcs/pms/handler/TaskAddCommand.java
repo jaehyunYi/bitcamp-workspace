dd326acb26786d34bbb99b5561ff9ef37e01e297
jaehyunYi <rmrmf@naver.com>
2020-11-03 10:42:55 +0900

    task변경

--- bitcamp-java-project-client/src/main/java/com/eomcs/pms/handler/TaskAddCommand.java
+++ bitcamp-java-project-client/src/main/java/com/eomcs/pms/handler/TaskAddCommand.java
@@ -3,6 +3,8 @@ package com.eomcs.pms.handler;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
+import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.domain.Task;
 import com.eomcs.util.Prompt;
 
@@ -19,25 +21,26 @@ public class TaskAddCommand implements Command {
     System.out.println("[작업 등록]");
 
     Task task = new Task();
-    task.setNo(Prompt.inputInt("번호? "));
     task.setContent(Prompt.inputString("내용? "));
     task.setDeadline(Prompt.inputDate("마감일? "));
     task.setStatus(Prompt.inputInt("상태?\n0: 신규\n1: 진행중\n2: 완료\n> "));
 
     while (true) {
-      String name = Prompt.inputString("담당자?(취소: 빈 문자열) ");
+        String name = Prompt.inputString("담당자?(취소: 빈 문자열) ");
 
-      if (name.length() == 0) {
-        System.out.println("작업 등록을 취소합니다.");
-        return;
-      } else if (memberListCommand.findByName(name) != null) {
-        task.setOwner(name);
-        break;
+        if (name.length() == 0) {
+          System.out.println("작업 등록을 취소합니다.");
+          return;
+        } else {
+          Member member = memberListCommand.findByName(name);
+          if (member == null) {
+            System.out.println("등록된 회원이 아닙니다.");
+            continue;
+          }
+          task.setOwner(member);
+          break;
+        }
       }
-
-      System.out.println("등록된 회원이 아닙니다.");
-    }
-
     try (Connection con = DriverManager.getConnection(
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
         PreparedStatement stmt = con.prepareStatement(
@@ -46,7 +49,7 @@ public class TaskAddCommand implements Command {
 
       stmt.setString(1, task.getContent());
       stmt.setDate(2, task.getDeadline());
-      stmt.setString(3, task.getOwner());
+      stmt.setInt(3, task.getOwner().getNo());
       stmt.setInt(4, task.getStatus());
       stmt.executeUpdate();
 
