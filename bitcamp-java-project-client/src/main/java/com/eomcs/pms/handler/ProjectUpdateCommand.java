bf2fdce9acd26858e31c433186031205340eee70
jaehyunYi <rmrmf@naver.com>
2020-11-16 20:55:52 +0900

    .

--- bitcamp-java-project-client/src/main/java/com/eomcs/pms/handler/ProjectUpdateCommand.java
+++ bitcamp-java-project-client/src/main/java/com/eomcs/pms/handler/ProjectUpdateCommand.java
@@ -40,22 +40,6 @@ public class ProjectUpdateCommand implements Command {
       project.setEndDate(Prompt.inputDate(String.format(
           "종료일(%s)? ", project.getEndDate())));
 
-      // 프로젝트에 참여할 회원 정보를 담는다.
-      List<Member> members = new ArrayList<>();
-      while (true) {
-        String name = Prompt.inputString("팀원?(완료: 빈 문자열) ");
-        if (name.length() == 0) {
-          break;
-        } else {
-          Member member = memberDao.findByName(name);
-          if (member == null) {
-            System.out.println("등록된 회원이 아닙니다.");
-            continue;
-          }
-          members.add(member);
-        }
-      }
-      project.setMembers(members);
 
       String response = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
       if (!response.equalsIgnoreCase("y")) {
