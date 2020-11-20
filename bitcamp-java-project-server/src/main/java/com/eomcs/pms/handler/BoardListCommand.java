9ae2c26f9b2addbd3370e49f8e9d41b8fb2256b4
jaehyunYi <rmrmf@naver.com>
2020-11-20 18:20:30 +0900

    servlet 초안

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/handler/BoardListCommand.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/handler/BoardListCommand.java
@@ -1,43 +1,53 @@
 package com.eomcs.pms.handler;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.pms.service.BoardService;
 
-@CommandAnno("/board/list")
-public class BoardListCommand implements Command {
-
-  BoardService boardService;
-
-  public BoardListCommand(BoardService boardService) {
-    this.boardService = boardService;
-  }
+@WebServlet("/board/list")
+public class BoardListCommand implements Servlet {
+  private static final long serial
 
 
-  @Override
-  public void execute(Request request) {
-    PrintWriter out = request.getWriter();
+@Override
+public void service(ServletRequest request, ServletResponse response)
+    throws ServletException, IOException {
+  
+  ServletContext ctx = request.getServletContext();
+  BoardService boardService = (BoardService) ctx.getAttribute("boardService");
+  
+  response.setContentType("text/plain;charset=UTF-8");
+      
+  PrintWriter out = response.getWriter();
 
-    try {
-      out.println("[게시물 목록]");
+  try {
+    out.println("[게시물 목록]");
 
-      List<Board> list = boardService.list();
+    List<Board> list = boardService.list();
 
-      out.println("번호, 제목, 작성자, 등록일, 조회수");
+    out.println("번호, 제목, 작성자, 등록일, 조회수");
 
-      for (Board board : list) {
-        out.printf("%d, %s, %s, %s, %d\n",
-            board.getNo(),
-            board.getTitle(),
-            board.getWriter().getName(),
-            board.getRegisteredDate(),
-            board.getViewCount());
-      }
-    } catch (Exception e) {
-      out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
-      e.printStackTrace();
+    for (Board board : list) {
+      out.printf("%d, %s, %s, %s, %d\n",
+          board.getNo(),
+          board.getTitle(),
+          board.getWriter().getName(),
+          board.getRegisteredDate(),
+          board.getViewCount());
     }
+  } catch (Exception e) {
+    out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
+    e.printStackTrace();
   }
-
+  
+}
+  
 }
