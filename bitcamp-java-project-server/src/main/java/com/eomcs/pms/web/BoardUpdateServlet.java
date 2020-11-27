fe7b6ee3dad1d075e1d59fb666cacde22da53df0
jaehyunYi <rmrmf@naver.com>
2020-11-27 20:12:06 +0900

    .

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/web/BoardUpdateServlet.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/web/BoardUpdateServlet.java
@@ -9,32 +9,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import com.eomcs.pms.domain.Board;
-import com.eomcs.pms.domain.Member;
 import com.eomcs.pms.service.BoardService;
 
-@WebServlet("/board/add")
-public class BoardAddServlet extends HttpServlet {
+@WebServlet("/board/update")
+public class BoardUpdateServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
+    request.setCharacterEncoding("UTF-8");
+
     ServletContext ctx = request.getServletContext();
     BoardService boardService =
         (BoardService) ctx.getAttribute("boardService");
-
-    // 클라이언트가 POST 요청할 때 보낸 데이터를 읽는다.
-    request.setCharacterEncoding("UTF-8");
-
-    Board board = new Board();
-    board.setTitle(request.getParameter("title"));
-    board.setContent(request.getParameter("content"));
-
-    // 회원 정보가 들어있는 세션 객체를 얻는다.
-    HttpSession session = request.getSession();
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -42,30 +32,35 @@ public class BoardAddServlet extends HttpServlet {
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
-    out.println("<meta http-equiv='Refresh' content='1;list'>");
-    out.println("<title>게시글등록</title></head>");
+    out.println("<meta http-equiv='Refresh' content='1;url=list'>");
+    out.println("<title>게시글변경</title></head>");
     out.println("<body>");
     try {
-      out.println("<h1>게시물 등록</h1>");
+      out.println("<h1>게시물 변경</h1>");
 
-      //Member loginUser = (Member) session.getAttribute("loginUser");
-      Member loginUser = new Member();
-      loginUser.setNo(1);
+      Board board = new Board();
+      board.setNo(Integer.parseInt(request.getParameter("no")));
+      board.setTitle(request.getParameter("title"));
+      board.setContent(request.getParameter("content"));
+      int count = boardService.update(board);
 
-      board.setWriter(loginUser);
+      if (count == 0) {
+        out.println("<p>해당 번호의 게시글이 없습니다.</p>");
 
-      boardService.add(board);
-
-      out.println("게시글을 등록하였습니다.");
+      } else {
+        out.println("<p>게시글을 변경하였습니다.</p>");
+      }
 
     } catch (Exception e) {
-      out.printf("<p>작업 처리 중 오류 발생! - %s</p>\n", e.getMessage());
+      out.println("<h2>작업 처리 중 오류 발생!</h2>");
+      out.printf("<pre>%s</pre>\n", e.getMessage());
 
       StringWriter errOut = new StringWriter();
       e.printStackTrace(new PrintWriter(errOut));
-
+      out.println("<h3>상세 오류 내용</h3>");
       out.printf("<pre>%s</pre>\n", errOut.toString());
     }
+
     out.println("</body>");
     out.println("</html>");
   }
