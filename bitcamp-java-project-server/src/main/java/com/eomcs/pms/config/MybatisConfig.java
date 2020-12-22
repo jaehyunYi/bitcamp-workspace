ab4510d79d66636a8864b2ce4a2c153621295d38
jaehyunYi <rmrmf@naver.com>
2020-12-22 20:59:38 +0900

    .

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/MybatisConfig.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/MybatisConfig.java
@@ -1,47 +1,27 @@
 package com.eomcs.pms.config;
 
 import javax.sql.DataSource;
+import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
 
 
-@PropertySource("classpath:com/eomcs/pms/config/jdbc.properties")
 @MapperScan("com.eomcs.pms.dao")
 public class MybatisConfig {
-
-  @Bean
-  public DataSource dataSource(
-      @Value("${jdbc.driver}") String jdbcDriver,
-      @Value("${jdbc.url}") String jdbcUrl,
-      @Value("${jdbc.username}") String jdbcUsername,
-      @Value("${jdbc.password}") String jdbcPassword) {
-    DriverManagerDataSource ds = new DriverManagerDataSource();
-    ds.setDriverClassName(jdbcDriver);
-    ds.setUrl(jdbcUrl);
-    ds.setUsername(jdbcUsername);
-    ds.setPassword(jdbcPassword);
-    return ds;
-  }
-
-  @Bean
-  public PlatformTransactionManager transactionManager(
-      DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
-  }
 
   @Bean
   public SqlSessionFactory sqlSessionFactory(
       DataSource dataSource, // DB 커넥션풀
       ApplicationContext appCtx // Spring IoC 컨테이너
       ) throws Exception {
+
+    // Log4J2 기능 활성화시키기
+    // => 로그 출력 형식은 .properties 파일이나 .xml 파일로 설정한다.
+    LogFactory.useLog4J2Logging();
+
     SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
     sqlSessionFactoryBean.setDataSource(dataSource);
     sqlSessionFactoryBean.setTypeAliasesPackage("com.eomcs.pms.domain");
